5023d9c400c37d2e620c0656f0f3db96a4a9dec9
jmtomeX <jmtomeX@gmail.com>
2020-02-27 12:32:15 +0100

    Decodificando token de usuario

---
+++ api/middlewares/authenticated.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const jwt = require("jwt-simple");
+const moment = require("moment");
+const secret = "clave_secreta_cualquira_6gsdfgl45C344_%ldsnalfkm";
+// hasta que no se llegue al next no sale de la función
+exports.ensureAuth = function(req, res, next) {
+  // el token llega en una cabecera
+  if (!req.headers.authorization) {
+    return res
+      .status(403)
+      .send({ message: "La petición no tiene la cabecera de autenticación." });
+  }
+  // reemplazamos cualquier comilla simple o doble que tenga el string por nada
+  var token = req.headers.authorization.replace(/['"]+/g, "");
+  // decoficar el token
+  try {
+    var payload = jwt.decode(token, secret);
+    if (payload.exp <= moment.unix()) {
+      return res.status(401).send({
+        message: "El token ha expirado"
+      });
+    }
+  } catch (ex) {
+    return res.status(404).send({
+        message: "El token no es válido"
+      });
+  }
+  // tenemos todos lo datos
+  req.user = payload;
+  next();
+};
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,10 +2,12 @@
 'user strict'
 const express = require("express");
 var UserController = require('../controllers/user');
+var middleware_auth = require('../middlewares/authenticated');
 // para tener acceso a los metodos GET,POST....
 var api = express.Router();
+
 api.get('/home', UserController.home);
-api.get('/pruebas', UserController.pruebas);
+api.get('/pruebas', middleware_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
